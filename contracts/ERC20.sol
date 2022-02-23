dd0e6ec46c7781281bd3fac72c88b06356e88bc0
yoru.lee <yoru.lee7@gmail.com>
2022-02-23 14:13:28 +0900

    truffle init & ERC20

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.10;
 
+
 interface ERC20Interface {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
