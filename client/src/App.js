e0ee59f4cb0dc7f52d971eeb1aaa068a285aadd7
aka773 <alkaloid77@naver.com>
2022-02-23 16:57:57 +0900

    Navbar 테일윈드 CSS 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
-import React, {  } from 'react';
-import { Route, Routes } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 
 import Home from './pages/Home';
 import Create from './pages/Create';
 import MyPage from './pages/MyPage';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 import './App.css';
 
 function App() {
 	return (
-		<Routes>
-			<Route exact path='/' element={<Home />} />
-			<Route path='/create' element={<Create />} />
-			<Route path='/mypage' element={<MyPage />} />
-		</Routes>
+		<div className='App'>
+			<Navbar />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/create' element={<Create />} />
+				<Route path='/mypage' element={<MyPage />} />
+			</Routes>
+			<Footer />
+		</div>
 	);
 }
 
