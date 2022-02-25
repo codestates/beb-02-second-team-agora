6489bef0a9e82d401e313899160d007520362ca3
aka773 <alkaloid77@naver.com>
2022-02-25 17:22:10 +0900

    Home화면 목업

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -22,6 +22,9 @@ const MyPage = () => {
 					<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 				</div>
 			</div>
+			<div>
+				<h1> My NFT List </h1>
+			</div>
 		</div>
 	);
 
